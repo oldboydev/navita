9e3628d9301f4ad1dad8227772a72519328c1dde
oldboydev <pm@oldboy.dev>
2020-09-19 08:38:25 -0300

    feat: discovery service config done

---
+++ discover-service/src/main/java/dev/oldboy/navita/discover/DiscoverServiceApplication.java
@@ -0,0 +1,15 @@
+package dev.oldboy.navita.discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServiceApplication.class, args);
+	}
+
+}
---
+++ discover-service/src/test/java/dev/oldboy/navita/discover/DiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oldboy.navita.discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
